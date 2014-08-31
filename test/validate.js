1d8e6e9fe429e88d3d421acd6e2d8af9545ca4bc
jtowers <jtowers@umail.iu.edu>
2014-08-31 00:11:03 +0000

    added validation module and a new validation method

---
+++ test/validate.js
@@ -0,0 +1,69 @@
+var should = require('chai').should();
+var blanket = require('blanket');
+var Authr = require('../index.js');
+
+describe.only('validation module', function () {
+  var authr;
+  beforeEach(function (done) {
+    config = {
+      db: {
+        type: 'nedb'
+      },
+      user: {
+        username: 'username',
+        password: 'password',
+        account_locked: 'account_locked',
+        account_locked_until: 'account_locked_until',
+        account_failed_attempts: 'account_failed_attempts',
+        account_last_failed_attempt: 'account_last_failed_attempt',
+        email_address: 'account_username',
+        email_verified: 'email_verified',
+        email_verification_hash: 'email_verification_hash',
+        email_verification_hash_expires: 'email_verification_expires'
+      },
+      security: {
+        hash_password: true,
+        hash_salt_factor: 1, // Hash factor reduced to reduce test times
+        max_failed_login_attempts: 10,
+        reset_attempts_after_minutes: 5,
+        lock_account_for_minutes: 30,
+        email_verification: true,
+        email_verification_expiration_hours: 12
+      },
+        custom: {
+            org :{
+                presence:true,
+                unique:true
+            },
+            username: {
+                unique:true
+            },
+            password:{
+                presence:true
+            }
+        }
+    };
+
+    authr = new Authr(config);
+    done();
+  });
+  afterEach(function (done) {
+    authr.config.Adapter.resetCollection(function (err) {
+      authr = null;
+      done();
+    });
+  });
+
+it('should validate', function(done){
+    var signup = {
+        username:'test',
+        password: 'test',
+        org: 'required'
+    };
+    authr.validate(signup, function(err, _signup){
+        should.not.exist(err);
+        done();
+    });
+});
+
+});
